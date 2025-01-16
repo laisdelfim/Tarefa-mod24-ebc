386593ff2660db6ed946c26847c4fff85f38088d
laisdelfim <159438320+laisdelfim@users.noreply.github.com>
2025-01-16 06:07:33 -0300

    Update ClienteDaoMock.java

--- src/dao/ClienteDaoMock.java
+++ src/dao/ClienteDaoMock.java
@@ -1,9 +1,24 @@
 package dao;
 
-public class ClienteDaoMock implements IClienteDao {
+public class ClienteDaoMock implements ClienteDao {
 
-  @Override
-  public String salvar() {
-    return null;
-  }
+    @Override
+    public String salvar() {
+        return "Sucesso";
+    }
+
+    @Override
+    public String buscar() {
+        return "Sucesso";
+    }
+
+    @Override
+    public String excluir() {
+        return "Sucesso";
+    }
+
+    @Override
+    public String atualizar() {
+        return "Sucesso";
+    }
 }
